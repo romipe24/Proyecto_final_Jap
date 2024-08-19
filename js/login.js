eff1b3cf4c0ab9f2f8360c7cf09427cb99d10a6b
leiarodriguez-3103 <leilarodriguez3103@gmail.com>
2024-08-19 11:44:37 -0300

    js login

---
+++ js/login.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.querySelector("form");
+    const createAccountButton = document.querySelector(".btn-secondary");
+
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        
+        if (username === "" || password === "") {
+            alert("Por favor, completa todos los campos.");
+            return;
+        }
+
+        
+        if (username === "usuario" && password === "contraseña") {
+            alert("Inicio de sesión exitoso");
+            window.location.href = "index.html"; 
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+    });
+
+    
+    createAccountButton.addEventListener("click", function() {
+        window.location.href = "create_account.html"; // pagina ejemplo, hay que hacer una pagina de crear usuario?
+    });
+});
