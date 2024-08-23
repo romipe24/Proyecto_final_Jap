fddba7709856f39a606d9dc339cb67b97d05afc4
romipe24 <84944787+romipe24@users.noreply.github.com>
2024-08-22 21:41:34 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,4 +1,3 @@
-
 const DATA_URL = "//japceibal.github.io/emercado-api/cats_products/101.json";
 
 let getJSONData = function(url) {
@@ -41,7 +40,7 @@ function mostrarDATA_URL(array) {
           
                
           
-                 <div class="col">
+                <div class="col">
                 <div class="card h-100">
                     <img src="${element.image}" class="card-img-top" alt="Lamborghini">
                     <div class="card-body">
@@ -60,64 +59,6 @@ function mostrarDATA_URL(array) {
             `;
         });
     }
-}
+};
 
-
-
-const DATA_URL =  "//japceibal.github.io/emercado-api/cats_products/101.json"
-let getJSONData = function(url){
-let result = {};
-return fetch(url)
-.then(response =>{
-    if (response.ok){
-        return response.json();
-    }else{
-        throw Error(response.statusText);
-    }
-    
-})
-.then(function(response) {
-    result.status = 'ok';
-    result.data = response;
-    return result; 
-})
-.catch(function(error) {
-    result.status = 'error';
-    result.data = error;
-    return result;
-
-});
-
-}
-document.addEventListener("DomContentLoaded", function(){
-    getJSONData(DATA_URL).then(function(resultado){
-        if(resultado.status = 'ok'){
-            console.log(resultado.data);
-            mostrarDATA_URL(resultado.data);
-        }
-    })
-})
-function mostrarDATA_URL (array) {
-            let mostrar = document.getElementsByClassName("autosinner");
-           
-            array.forEach(element => {
-                
-  mostrar.innerHTML +=
-        ` <div class="col"
-        <div class="card h-100" >
-          <img href= ${element.product.image} > 
-        <div class="card-body">
-         <h1 class="text-center py-4 card-tittle"> ${element.product.name}  </h1>
-        <p class "card-text"> ${element.product.name} </P>
-          <p class "card-text"> ${element.product.description} </P>
-          <p class "card-text"> ${element.product.cost} ${element.product.currency} </P>
-          </div>
-          <div class="card-footer text-end">
-          <p class "card-text"> ${element.product.soldCount} </P>
-          </div>
-         </div> 
-         </div>
-          `;
-});
-}
 
