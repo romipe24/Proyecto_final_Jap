2cfaefcbaa23c4c6ca6af9d5eb31dc4b42fd23ee
noeliasantanaroda <noelia.sroda@gmail.com>
2024-08-29 18:30:59 -0300

    desafiate todas las pag

--- js/sell.js
+++ js/sell.js
@@ -147,3 +147,31 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
     });
 });
+ // Verificar si el usuario está autenticado
+ const user = localStorage.getItem('user');
+
+ // Verificar si ya se mostró la alerta
+ const confirmShown = sessionStorage.getItem('confirmShown');
+
+
+ if (!user && !confirmShown) {
+      // Muestra la alerta
+     const userConfirmed = confirm('No has iniciado sesión. ¿Deseas iniciar sesión ahora?');
+     
+
+     if (userConfirmed) {
+         // Redirigir al login si el usuario desea iniciar sesión
+         window.location.href = 'login.html';
+     } 
+
+     // Marca que la alerta ya se ha mostrado en la sesión actual
+     sessionStorage.setItem('confirmShown', 'true');
+
+ };
+ if (user) {
+  document.getElementById('user-name').textContent = `Bienvenid@, ${user}`;
+}
+  //esto es para cuando le pongamos boton de cerrar sesion
+  // document.getElementById('logoutButton').addEventListener('click', function() {
+  //     // Eliminar la sesión de LocalStorage
+  //     localStorage.removeItem('user');
