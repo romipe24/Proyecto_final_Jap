4a2c34d5e81de3ffa0c698836c91d28485997195
joselhinsanchezz <yose.sanchezz@gmail.com>
2024-10-17 19:48:22 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -43,7 +43,6 @@ document.addEventListener("DOMContentLoaded", function() {
             // Si no hay perfil guardado, mostrar el email como nombre
             document.getElementById('user-name').textContent = `Bienvenid@, ${user}`;
         }
-
     }
 
     // Modo oscuro
