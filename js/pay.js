0face00dc9bd26107687762046cc5bb3e4efe844
Joselhin <yose.sanchezz@gmail.com>
2024-11-16 12:17:33 -0300

    Revert "Merge branch 'main' of https://github.com/romipe24/Proyecto_final_Jap"
    This reverts commit fe7c8745da30508408db80a21df0474507bfc3ec, reversing
    changes made to 1feaa6174084c3a66aff8fd690c2ac05605f395d.

--- js/pay.js
+++ js/pay.js
@@ -1,3 +1,183 @@
+// Cargar los productos del localStorage
+let cartProducts = JSON.parse(localStorage.getItem('cartProducts')) || [];
+const cartContent = document.getElementById('cart-content');
+const totalElementUSD = document.getElementById('totalUSD');
+const totalElementUYU = document.getElementById('totalUYU');
+const cartBadge = document.getElementById('cart-badge');
+
+// Función para convertir precios del formato con punto decimal a número entero
+const convertPrice = (price) => {
+    return typeof price === 'string' ? parseFloat(price.replace(/\./g, '')) : price;
+};
+
+// Función para formatear números con comas
+const formatNumber = (num) => num.toLocaleString();
+
+// Función para actualizar el badge del carrito
+const updateCartBadge = () => {
+    const totalQuantity = cartProducts.reduce((total, product) => total + product.quantity, 0);
+    cartBadge.textContent = totalQuantity;
+};
+
+// Actualizar el badge cuando se cargue la página
+updateCartBadge();
+
+if (cartProducts.length > 0) {
+    let total = 0;
+
+    cartProducts.forEach((product, index) => {
+        product.price = convertPrice(product.price);
+
+        const updateTotal = () => {
+            let subtotalUSD = 0;
+            let subtotalUYU = 0;
+            cartProducts.forEach(product => {
+                if (product.currency === 'USD') {
+                    subtotalUSD += product.price * product.quantity;
+                } else if (product.currency === 'UYU') {
+                    subtotalUYU += product.price * product.quantity;
+                }
+            });
+
+            totalElementUSD.style.display = subtotalUSD > 0 ? 'block' : 'none';
+            totalElementUSD.textContent = `Total USD ${formatNumber(subtotalUSD.toFixed(2))}`;
+            totalElementUYU.style.display = subtotalUYU > 0 ? 'block' : 'none';
+            totalElementUYU.textContent = `Total UYU ${formatNumber(subtotalUYU.toFixed(2))}`;
+        };
+
+        const productDiv = document.createElement('div');
+        productDiv.className = "cart-item";
+        productDiv.innerHTML = `
+            <img src="${product.image}" alt="${product.name}">
+            <div class="cart-item-details">
+                <h3>${product.name}</h3>
+                <p>Precio: ${product.currency} ${product.price}</p>
+                <p>Subtotal: <span id="subtotal-${index}">${product.currency} ${(product.price * product.quantity).toFixed(2)}</span></p>
+            </div>
+            <div class="cart-actions">
+                <div class="input-group">
+                    <button class="btn btn-outline-secondary" type="button" id="decrease-${index}">-</button>
+                    <input type="text" class="form-control text-center" id="quantity-${index}" value="${product.quantity}" readonly>
+                    <button class="btn btn-outline-secondary" type="button" id="increase-${index}">+</button>
+                </div>
+                <button class="btn btn-danger" id="remove-${index}"><i class="fas fa-trash-alt"></i></button>
+            </div>
+        `;
+
+        cartContent.appendChild(productDiv);
+        updateTotal();
+
+        const increaseButton = document.getElementById(`increase-${index}`);
+        const decreaseButton = document.getElementById(`decrease-${index}`);
+        const quantityInput = document.getElementById(`quantity-${index}`);
+
+        increaseButton.addEventListener('click', () => {
+            product.quantity++;
+            quantityInput.value = product.quantity;
+            document.getElementById(`subtotal-${index}`).textContent = `${product.currency} ${formatNumber((product.price * product.quantity).toFixed(2))}`;
+            localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
+            updateTotal();
+            updateCartBadge(); // Actualizar el badge
+        });
+
+        decreaseButton.addEventListener('click', () => {
+            if (product.quantity > 1) {
+                product.quantity--;
+                quantityInput.value = product.quantity;
+                document.getElementById(`subtotal-${index}`).textContent = `${product.currency} ${formatNumber((product.price * product.quantity).toFixed(2))}`;
+                localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
+                updateTotal();
+                updateCartBadge(); // Actualizar el badge
+            }
+        });
+
+        document.getElementById(`remove-${index}`).addEventListener('click', () => {
+            cartProducts.splice(index, 1);
+            localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
+            window.location.reload(); // Recargar la página para refrescar el carrito
+            updateCartBadge(); // Actualizar el badge
+        });
+    });
+} else {
+    cartContent.innerHTML = `<p class="alert alert-warning">El carrito está vacío.</p>`;
+    totalElementUSD.textContent = '';
+    totalElementUYU.textContent = '';
+}
+
+// Vaciar carrito
+document.getElementById('clear-cart').addEventListener('click', () => {
+    localStorage.removeItem('cartProducts');
+    cartProducts = [];
+    cartContent.innerHTML = `<p class="alert alert-warning">El carrito está vacío.</p>`;
+    totalElementUSD.textContent = '';
+    totalElementUYU.textContent = '';
+    updateCartBadge(); // Actualizar el badge
+});
+
+// ** Costos en tiempo real**
+
+const shippingTypeElement = document.getElementById('shippingType');
+const subtotalElement = document.getElementById('subtotal');
+const shippingCostElement = document.getElementById('shippingCost');
+
+// Actualiza los costos (Subtotal, Envío y Total)
+const updateCosts = () => {
+    let subtotalUSD = 0;
+    let subtotalUYU = 0;
+
+    cartProducts.forEach(product => {
+        if (product.currency === 'USD') {
+            subtotalUSD += product.price * product.quantity;
+        } else if (product.currency === 'UYU') {
+            subtotalUYU += product.price * product.quantity;
+        }
+    });
+
+    // Actualizamos el subtotal
+    document.getElementById('subtotalUSD').textContent = `USD ${formatNumber(subtotalUSD.toFixed(2))}`;
+    document.getElementById('subtotalUYU').textContent = `UYU ${formatNumber(subtotalUYU.toFixed(2))}`;
+
+    // Cálculo del costo de envío basado en el tipo de envío seleccionado
+    let shippingCostUSD = 0;
+    let shippingCostUYU = 0;
+    const shippingType = shippingTypeElement.value;
+    if (shippingType === 'premium') {
+        shippingCostUSD = subtotalUSD * 0.15; // 15% de costo adicional
+        shippingCostUYU = subtotalUYU * 0.15; 
+    } else if (shippingType === 'express') {
+        shippingCostUSD = subtotalUSD * 0.07; // 7% de costo adicional
+        shippingCostUYU = subtotalUYU * 0.07; 
+    } else if (shippingType === 'standard') {
+        shippingCostUSD = subtotalUSD * 0.05; // 5% de costo adicional
+        shippingCostUYU = subtotalUYU * 0.05; 
+    }
+
+    document.getElementById('shippingCostUSD').textContent = `USD ${formatNumber(shippingCostUSD.toFixed(2))}`;
+    document.getElementById('shippingCostUYU').textContent = `UYU ${formatNumber(shippingCostUYU.toFixed(2))}`;
+
+    // Calculamos el total
+    const totalCost = subtotalUSD + shippingCostUSD;
+    document.getElementById('totalCostUSD').textContent = `USD ${formatNumber(totalCost.toFixed(2))}`;
+    totalElementUSD.textContent = `Total USD: ${formatNumber(totalCost.toFixed(2))}`;
+
+    const totalCostUYU = subtotalUYU + shippingCostUYU;
+    document.getElementById('totalCostUYU').textContent = `UYU ${formatNumber(totalCostUYU.toFixed(2))}`;
+    totalElementUYU.textContent = `Total UYU: ${formatNumber(totalCostUYU.toFixed(2))}`;
+};
+
+// Actualizar los costos al cambiar el tipo de envío
+shippingTypeElement.addEventListener('change', updateCosts);
+
+// Actualizamos los costos cuando se cargue la página
+if (cartProducts.length > 0) {
+    updateCosts();
+}
+
+// ** Finalizar Compra**
+document.getElementById("finalizePurchase").addEventListener("click", function() {
+    alert("Compra finalizada. ¡Gracias por tu compra!");
+});
+
 // ** Navegación de categorías**
 document.getElementById("autos").addEventListener("click", function() {
     localStorage.setItem("catID", 101);
@@ -67,209 +247,3 @@ if (user) {
         document.getElementById('user-name').textContent = `Bienvenid@, ${user}`;
     }
 }
-
-// Cargar los productos del localStorage
-let cartProducts = JSON.parse(localStorage.getItem('cartProducts')) || [];
-const cartContent = document.getElementById('cart-content');
-const totalElementUSD = document.getElementById('totalUSD');
-const totalElementUYU = document.getElementById('totalUYU');
-const cartBadge = document.getElementById('cart-badge');
-
-// Función para convertir precios del formato con punto decimal a número entero
-const convertPrice = (price) => {
-    return typeof price === 'string' ? parseFloat(price.replace(/\./g, '')) : price;
-};
-
-// Función para formatear números con comas
-const formatNumber = (num) => num.toLocaleString();
-
-// Función para actualizar el badge del carrito
-const updateCartBadge = () => {
-    const totalQuantity = cartProducts.reduce((total, product) => total + product.quantity, 0);
-    cartBadge.textContent = totalQuantity;
-};
-
-// Actualizar el badge cuando se cargue la página
-updateCartBadge();
-
-if (cartProducts.length > 0) {
-    cartProducts.forEach((product, index) => {
-        product.price = convertPrice(product.price);
-
-        const productDiv = document.createElement('div');
-        productDiv.className = "cart-item";
-        productDiv.innerHTML = `
-            <img src="${product.image}" alt="${product.name}" class="img-fluid mx-auto d-block mb-2">
-            <div class="cart-item-details">
-                <h3>${product.name}</h3>
-                  <p>Cantidad: ${product.quantity}</p>
-                <p>Precio: ${product.currency} ${product.price}</p>
-              
-            </div>
-        `;
-
-        cartContent.appendChild(productDiv);
-    });
-}
-
-// ** Costos en tiempo real**
-const shippingTypeElement = document.getElementById('shippingType');
-const subtotalElement = document.getElementById('subtotal');
-const shippingCostElement = document.getElementById('shippingCost');
-
-// Actualiza los costos (Subtotal, Envío y Total)
-const updateCosts = () => {
-    let subtotalUSD = 0;
-    let subtotalUYU = 0;
-
-    cartProducts.forEach(product => {
-        if (product.currency === 'USD') {
-            subtotalUSD += product.price * product.quantity;
-        } else if (product.currency === 'UYU') {
-            subtotalUYU += product.price * product.quantity;
-        }
-    });
-
-    // Actualizamos el subtotal
-    document.getElementById('subtotalUSD').textContent = `USD ${formatNumber(subtotalUSD.toFixed(2))}`;
-    document.getElementById('subtotalUYU').textContent = `UYU ${formatNumber(subtotalUYU.toFixed(2))}`;
-
-    // Cálculo del costo de envío basado en el tipo de envío seleccionado
-    let shippingCostUSD = 0;
-    let shippingCostUYU = 0;
-    const shippingType = shippingTypeElement.value;
-    if (shippingType === 'premium') {
-        shippingCostUSD = subtotalUSD * 0.15; // 15% de costo adicional
-        shippingCostUYU = subtotalUYU * 0.15; 
-    } else if (shippingType === 'express') {
-        shippingCostUSD = subtotalUSD * 0.07; // 7% de costo adicional
-        shippingCostUYU = subtotalUYU * 0.07; 
-    } else if (shippingType === 'standard') {
-        shippingCostUSD = subtotalUSD * 0.05; // 5% de costo adicional
-        shippingCostUYU = subtotalUYU * 0.05; 
-    }
-
-    document.getElementById('shippingCostUSD').textContent = `USD ${formatNumber(shippingCostUSD.toFixed(2))}`;
-    document.getElementById('shippingCostUYU').textContent = `UYU ${formatNumber(shippingCostUYU.toFixed(2))}`;
-
-    // Calculamos el total
-    const totalCostUSD = subtotalUSD + shippingCostUSD;
-    document.getElementById('totalCostUSD').textContent = `USD ${formatNumber(totalCostUSD.toFixed(2))}`;
-    totalElementUSD.textContent = `Total USD: ${formatNumber(totalCostUSD.toFixed(2))}`;
-
-    const totalCostUYU = subtotalUYU + shippingCostUYU;
-    document.getElementById('totalCostUYU').textContent = `UYU ${formatNumber(totalCostUYU.toFixed(2))}`;
-    totalElementUYU.textContent = `Total UYU: ${formatNumber(totalCostUYU.toFixed(2))}`;
-};
-
-// Actualizar los costos al cambiar el tipo de envío
-shippingTypeElement.addEventListener('change', updateCosts);
-
-// Actualizamos los costos cuando se cargue la página
-if (cartProducts.length > 0) {
-    updateCosts();
-}
-
-// ** Mostrar y Ocultar Formulario de Pago **
-document.addEventListener('DOMContentLoaded', function() {
-    var paymentMethod = document.getElementById('paymentMethod');
-    var creditCardForm = document.getElementById('creditCardForm');
-    var bankTransferForm = document.getElementById('bankTransferForm');
-
-    // Mostrar formulario de tarjeta de crédito por defecto
-    creditCardForm.style.display = 'block';
-    bankTransferForm.style.display = 'none';
-
-    paymentMethod.addEventListener('change', function() {
-        if (paymentMethod.value === 'creditCard') {
-            creditCardForm.style.display = 'block';
-            bankTransferForm.style.display = 'none';
-        } else if (paymentMethod.value === 'bankTransfer') {
-            creditCardForm.style.display = 'none';
-            bankTransferForm.style.display = 'block';
-        }
-    });
-});
-
-// ** Finalizar Compra y Validación de Pago**
-// Variable para rastrear si el botón "direccion" fue clickeado
-let addressSubmitted = false;
-
- // Agregar evento al botón de guardar dirección
- document.getElementById("direccion").addEventListener("click", function () {
-    event.preventDefault();
-    // Obtener valores de los campos del formulario
-    const department = document.getElementById("department").value.trim();
-    const locality = document.getElementById("locality").value.trim();
-    const street = document.getElementById("street").value.trim();
-    const number = document.getElementById("number").value.trim();
-    const corner = document.getElementById("corner").value.trim();
-
-    // Validar que todos los campos estén completos
-    if (!department || !locality || !street || !number || !corner) {
-        alert("Por favor, completa todos los campos.");
-        return;
-    }
-
-    // Crear un objeto con los datos
-    const shippingAddress = {
-        department,
-        locality,
-        street,
-        number,
-        corner,
-    };
-
-    // Guardar el objeto en localStorage
-    localStorage.setItem("shippingAddress", JSON.stringify(shippingAddress));
-
-    // Mostrar una alerta de éxito
-    alert("Dirección guardada correctamente.");
-    addressSubmitted = true;
-});
-
-// Evento del botón "finalizePurchase"
-document.getElementById("finalizePurchase").addEventListener("click", function() {
-    // Verificar si el botón "direccion" fue clickeado
-    if (!addressSubmitted) {
-        alert('Por favor, confirma primero tu dirección de envío.');
-        return;
-    }
-
-    var paymentMethod = document.getElementById('paymentMethod').value;
-
-    if (paymentMethod === 'creditCard') {
-        var cardNumber = document.getElementById('cardNumber').value;
-        var cvv = document.getElementById('cvv').value;
-        var expiryDate = document.getElementById('expiryDate').value;
-        var [expiryMonth, expiryYear] = expiryDate.split('/').map(Number);
-
-        var currentYear = new Date().getFullYear() % 100;
-        var currentMonth = new Date().getMonth() + 1;
-
-        if (!cardNumber || !cvv || !expiryDate) {
-            alert('Por favor, completa todos los campos de la tarjeta de crédito.');
-            return;
-        }
-
-        if (expiryYear < currentYear || (expiryYear === currentYear && expiryMonth < currentMonth)) {
-            alert('Fecha de vencimiento no válida.');
-            return;
-        }
-    }
-
-    alert('Compra finalizada. ¡Gracias por tu compra!');
-    // Vaciar carrito
-        localStorage.removeItem('cartProducts');
-        cartProducts = [];
-        cartContent.innerHTML = `<p class="alert alert-warning">El carrito está vacío.</p>`;
-        totalElementUSD.textContent = '';
-        totalElementUYU.textContent = '';
-       updateCartBadge(); // Actualizar el badge
-       updateCosts();
-    //Rederigir al inicio
-        window.location.href = "index.html";
-});
-
-
-
